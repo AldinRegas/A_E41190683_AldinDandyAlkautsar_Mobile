5b55c1b8aa38890f0a35b06f5e7c214c7a9cd587
AldinRegas <72177162+AldinRegas@users.noreply.github.com>
2021-03-21 15:02:03 +0700

    LinierLayout

---
+++ LinierLayout/app/src/main/java/com/example/aldindandya/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.aldindandya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText to, Subject, Pesan;
+    Button Submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        to = findViewById(R.id.to);
+        Subject = findViewById(R.id.Subject);
+        Pesan = findViewById(R.id.Pesan);
+
+    }
+}
